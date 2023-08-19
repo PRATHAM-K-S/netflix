e9e710cf84e5f04d1cfb8b0d34310a82859750e6
PRATHAM-K-S <142659570+PRATHAM-K-S@users.noreply.github.com>
2023-08-19 18:01:12 +0530

    added files

---
+++ script.js
@@ -0,0 +1,9 @@
+let eng = document.getElementById(`eng`)
+eng.addEventListener(`click`,()=>{
+    document.getElementById(`select-language-content`).innerHTML = eng.innerHTML;
+})
+
+let hin = document.getElementById(`hin`)
+hin.addEventListener(`click`,()=>{
+    document.getElementById(`select-language-content`).innerHTML = hin.innerHTML;
+})
